555de12021905c60ad5229741a9549b39dc01897
Wilhelm Wie-Groenhof <wilhelwi@clab02.idi.ntnu.no>
2023-04-19 23:06:00 +0200

    json

--- project/yolov7-main/dataanalysis/find_aspect_ratios.py
+++ project/yolov7-main/dataanalysis/find_aspect_ratios.py
@@ -1,25 +1,37 @@
 import os
 import numpy as np
-from utils.general import *
 
-countries = {"China_Drone" : [0,0,0,0], "Chine_MotorBike": [0,0,0,0],"Czech": [0,0,0,0],"India": [0,0,0,0],"Japan": [0,0,0,0],"Norway": [0,0,0,0],"United_States": [0,0,0,0]}
+def xyxy2xywh(x):
+    # Convert nx4 boxes from [x1, y1, x2, y2] to [x, y, w, h] where xy1=top-left, xy2=bottom-right
+    y = np.copy(x)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2  # x center
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2  # y center
+    y[:, 2] = x[:, 2] - x[:, 0]  # width
+    y[:, 3] = x[:, 3] - x[:, 1]  # height
+    return y
+#"China_Drone" : np.array([0,0,0,0]), "Chine_MotorBike": np.array([0,0,0,0]),
+countries = {"Czech": np.array([0,0,0,0]),"India": np.array([0,0,0,0]),"Japan": np.array([0,0,0,0]),"Norway": np.array([0,0,0,0]),"United_States": np.array([0,0,0,0])}
 aspect_ratios_sum = np.array([0,0,0,0])
 total_label_sum = np.array([0,0,0,0])
-for key, item in countries:
+for i, (key, item) in enumerate(countries.items()):
     directory = f"./data/RDD2022/{key}/train/labels/"
     for filename in os.listdir(directory):
-        with open(filename, 'r') as f:
+        with open(directory + filename, 'r') as f:
             for line in f:
                 x = line.split()
                 type = int(x[0])
                 
-                [xmin, ymin, xmax, ymax] = int(x[1:5])
-                [x,y,w,h] = xyxy2xywh([xmin,ymin,xmax,ymax])
-                ar = w/h
+                [xmax, ymax, xmin, ymin] = [float(x[1]),float(x[2]),float(x[3]),float(x[4])]
+                w = abs(xmax-xmin)
+                h = abs(ymax-ymin)
+                if (h == 0): ar = 0
+                else: ar = w/h
+                
+                
 
                 aspect_ratios_sum[type] += ar
                 item[type] += 1
-                total_label_sum[type] += 0
+                total_label_sum[type] += 1
 
 aspect_ratios = aspect_ratios_sum/total_label_sum
 print(aspect_ratios)
